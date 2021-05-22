4b811b4deb8da489f915e2d37391fe6a08368f80
Shahid <mshahidgo@gmail.com>
2021-05-23 00:05:50 +0530

    Emmets

---
+++ TA 4/Emmets/index.js
@@ -0,0 +1,10 @@
+//print all the contents of current window
+    //console.log(window);
+
+// take two input from use and print the larger no
+
+var number1 = window.prompt("Enter Number1");
+var number2 = window.prompt("Enter Number2");
+
+var greater = number1>number2?number1:number2;
+console.log(greater);
